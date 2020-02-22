47bd2aa38f22a9fafa6a679ece947bcf06a15ad1
amoux <carlosdeveloper2@gmail.com>
2020-02-22 17:04:23 -0500

    updated version and added package installation setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+PACKAGES = find_packages()
+
+setup(
+    name='david-sentiment',
+    packages=PACKAGES,
+    version='0.1.0',
+    url='https://github.com/amoux/david-sentiment',
+    author='Carlos A. Segura Diaz De Leon',
+    author_email='carlosdeveloper2@gmail.com',
+    license='MIT',
+    zip_safe=False,
+)
